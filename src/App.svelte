4b0bb7f31a70b9b8d77f6d285754152d567448ae
Xinrea <xinreas@outlook.com>
2024-10-09 17:28:11 +0800

    feat: show version on main window title

--- src/App.svelte
+++ src/App.svelte
@@ -1,83 +1,84 @@
 <script lang="ts">
-  import Room from "./lib/Room.svelte";
-  import BSidebar from "./lib/BSidebar.svelte";
-  import Summary from "./lib/Summary.svelte";
-  import Setting from "./lib/Setting.svelte";
-  import Account from "./lib/Account.svelte";
-  import TitleBar from "./lib/TitleBar.svelte";
-  import Messages from "./lib/Messages.svelte";
-  import { platform } from "@tauri-apps/plugin-os";
-  let active = "#总览";
-  let room_count = 0;
-  let message_cnt = 0;
-  let use_titlebar = platform() == "windows";
+    import Room from "./lib/Room.svelte";
+    import BSidebar from "./lib/BSidebar.svelte";
+    import Summary from "./lib/Summary.svelte";
+    import Setting from "./lib/Setting.svelte";
+    import Account from "./lib/Account.svelte";
+    import TitleBar from "./lib/TitleBar.svelte";
+    import Messages from "./lib/Messages.svelte";
+    import About from "./lib/About.svelte";
+    import { platform } from "@tauri-apps/plugin-os";
+    let active = "#总览";
+    let room_count = 0;
+    let message_cnt = 0;
+    let use_titlebar = platform() == "windows";
 </script>
 
 <main>
-  {#if use_titlebar}
-    <TitleBar />
-  {/if}
-  <div class="wrap">
-    <div class="sidebar">
-      <BSidebar bind:activeUrl={active} {room_count} {message_cnt} />
-    </div>
-    <div class="content">
-      <!-- switch component by active -->
-      <div class="page" class:visible={active == "#总览"}>
-        <Summary />
-      </div>
-      <div class="h-full page" class:visible={active == "#直播间"}>
-        <Room bind:room_count />
-      </div>
-      <div class="h-full page" class:visible={active == "#消息"}>
-        <Messages bind:message_cnt />
-      </div>
-      <div class="h-full page" class:visible={active == "#账号"}>
-        <Account />
-      </div>
-      <div class="page" class:visible={active == "#自动化"}>
+    {#if use_titlebar}
+        <TitleBar />
+    {/if}
+    <div class="wrap">
+        <div class="sidebar">
+            <BSidebar bind:activeUrl={active} {room_count} {message_cnt} />
+        </div>
+        <div class="content">
+            <!-- switch component by active -->
+            <div class="page" class:visible={active == "#总览"}>
+                <Summary />
+            </div>
+            <div class="h-full page" class:visible={active == "#直播间"}>
+                <Room bind:room_count />
+            </div>
+            <div class="h-full page" class:visible={active == "#消息"}>
+                <Messages bind:message_cnt />
+            </div>
+            <div class="h-full page" class:visible={active == "#账号"}>
+                <Account />
+            </div>
+            <!-- <div class="page" class:visible={active == "#自动化"}>
         <div>自动化[开发中]</div>
-      </div>
-      <div class="page" class:visible={active == "#设置"}>
-        <Setting />
-      </div>
-      <div class="page" class:visible={active == "#关于"}>
-        <div>关于</div>
-      </div>
+      </div> -->
+            <div class="page" class:visible={active == "#设置"}>
+                <Setting />
+            </div>
+            <div class="page" class:visible={active == "#关于"}>
+                <About />
+            </div>
+        </div>
     </div>
-  </div>
 </main>
 
 <style>
-  .sidebar {
-    display: flex;
-    height: 100vh;
-  }
+    .sidebar {
+        display: flex;
+        height: 100vh;
+    }
 
-  .wrap {
-    display: flex;
-    flex-direction: row;
-    height: 100vh;
-    overflow: hidden;
-  }
+    .wrap {
+        display: flex;
+        flex-direction: row;
+        height: 100vh;
+        overflow: hidden;
+    }
 
-  .visible {
-    opacity: 1 !important;
-    max-height: fit-content !important;
-    transform: translateX(0) !important;
-  }
+    .visible {
+        opacity: 1 !important;
+        max-height: fit-content !important;
+        transform: translateX(0) !important;
+    }
 
-  .page {
-    opacity: 0;
-    max-height: 0;
-    transform: translateX(100%);
-    overflow: hidden;
-    transition:
-      opacity 0.5s ease-in-out,
-      transform 0.3s ease-in-out;
-  }
+    .page {
+        opacity: 0;
+        max-height: 0;
+        transform: translateX(100%);
+        overflow: hidden;
+        transition:
+            opacity 0.5s ease-in-out,
+            transform 0.3s ease-in-out;
+    }
 
-  .content {
-    height: 100vh;
-  }
+    .content {
+        height: 100vh;
+    }
 </style>
---
+++ src/lib/About.svelte
@@ -0,0 +1,15 @@
+<script type="ts">
+    import { getVersion } from "@tauri-apps/api/app";
+    import { getCurrentWebviewWindow } from "@tauri-apps/api/webviewWindow";
+    const appWindow = getCurrentWebviewWindow();
+    let version = "";
+    getVersion().then((v) => {
+        version = v;
+        appWindow.setTitle(`BiliBili ShadowReplay - v${version}`);
+        console.log(version);
+    });
+</script>
+
+<div class="p-8 pt-12 h-full overflow-auto">
+    当前版本: {version}
+</div>

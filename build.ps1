3bb215ae946bd70f7aea50e8e99926d57bac727b
Xinrea <xinreas@outlook.com>
2025-03-31 01:52:07 +0800

    ci/cd: update local build script

--- build.ps1
+++ build.ps1
@@ -3,6 +3,26 @@
 yarn tauri build
 yarn tauri build --debug
 
+# rename the builds, "bili-shadowreplay" to "bili-shadowreplay-cpu"
+Get-ChildItem -Path ./src-tauri/target/release/bundle/msi/ | ForEach-Object {
+    $newName = $_.Name -replace 'bili-shadowreplay', 'bili-shadowreplay-cpu'
+    Rename-Item -Path $_.FullName -NewName $newName
+}
+Get-ChildItem -Path ./src-tauri/target/release/bundle/nsis/ | ForEach-Object {
+    $newName = $_.Name -replace 'bili-shadowreplay', 'bili-shadowreplay-cpu'
+    Rename-Item -Path $_.FullName -NewName $newName
+}
+
+# rename the debug builds, "bili-shadowreplay" to "bili-shadowreplay-cpu"
+Get-ChildItem -Path ./src-tauri/target/debug/bundle/msi/ | ForEach-Object {
+    $newName = $_.Name -replace 'bili-shadowreplay', 'bili-shadowreplay-cpu'
+    Rename-Item -Path $_.FullName -NewName $newName
+}
+Get-ChildItem -Path ./src-tauri/target/debug/bundle/nsis/ | ForEach-Object {
+    $newName = $_.Name -replace 'bili-shadowreplay', 'bili-shadowreplay-cpu'
+    Rename-Item -Path $_.FullName -NewName $newName
+}
+
 # move the build to the correct location
 Move-Item ./src-tauri/target/release/bundle/msi/* ./src-tauri/target/
 Move-Item ./src-tauri/target/release/bundle/nsis/* ./src-tauri/target/
